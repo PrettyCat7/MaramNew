de7f1aca8ac4281bf08027efd95d540a863735df
maram <barbourmaram@gmail.com>
2023-11-09 13:18:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/maramnew/FireBaseServices.java
@@ -0,0 +1,37 @@
+package com.example.maramnew;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
+public class FireBaseServices {
+    private static FireBaseServices instance;
+    private FirebaseAuth auth;
+    private FirebaseFirestore fire;
+    private FirebaseStorage storage;
+
+    public static FireBaseServices getInstance() {
+        if (instance == null)
+            instance = new FireBaseServices();
+        return instance;
+    }
+
+    public FirebaseAuth getAuth() {
+        return auth;
+    }
+
+    public FirebaseFirestore getFire() {
+        return fire;
+    }
+
+    public FirebaseStorage getStorage() {
+        return storage;
+    }
+
+    public FireBaseServices() {
+        auth = FirebaseAuth.getInstance();
+        fire = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+    }
+}
